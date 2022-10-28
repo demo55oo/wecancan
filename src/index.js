8be1d2d5277835c6bb8e31976bc8ef453a8a8978
demo55oo <85605417+demo55oo@users.noreply.github.com>
2022-10-28 13:33:55 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import AppProvider from './store/AppProvider';
 import './i18n.js'
@@ -10,9 +10,9 @@ import './i18n.js'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <HashRouter>
-    <AppProvider>
+    <BrowserRouter>
       <App />
-    </AppProvider>
+    </BrowserRouter>
   </HashRouter>
 );
 
